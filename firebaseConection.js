b23ed8a14dd47c9434cf06e14fd7f7c663badcc8
Arthur Mendonça <109369137+ArthurMends777@users.noreply.github.com>
2023-12-07 19:38:47 -0300

    removing connection to my bank
    Update firebaseConection.js

--- firebaseConection.js
+++ firebaseConection.js
@@ -4,13 +4,9 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-dnDrH9FSG5AJo2unicuWdroEidctzdE",
-  authDomain: "tccfinancas-fd7a9.firebaseapp.com",
-  databaseURL: "https://tccfinancas-fd7a9-default-rtdb.firebaseio.com",
-  projectId: "tccfinancas-fd7a9",
-  storageBucket: "tccfinancas-fd7a9.appspot.com",
-  messagingSenderId: "613723245646",
-  appId: "1:613723245646:web:bf6e26fda11fe3ab1c071f"
+ /*
+   Coloque a conexão com seu banco aqui
+ */
 };
 
 // Initialize Firebase
